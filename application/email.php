d1d99aad2d970b5385041cfe597c8630f2ed63d2
xavierartot <artotal@gmail.com>
2014-08-29 10:36:46 -0500

    json_decode in email

--- application/email.php
+++ application/email.php
@@ -1,10 +1,17 @@
     <?php
     //echo var_dump($_POST);
+    $tab = array();
+    $tab[$_POST];
     $to = "artotal@gmail.com";
     $subject = "Hi!";
     $body = 'name: '.$_POST['name'];
     $body .= 'email: '.$_POST['email'];
-    $body .= 'message: '.$_POST['message'];
+    $body .= 'message: '.$_POST['message'].
+    ' /// '. 
+    $HTTP_RAW_POST_DATA
+    ' /// '. 
+    var_dump( json_decode($tab) ) . ' /// '. 
+    ' /// '; 
 
     if (mail($to, $subject, $body)) {
      echo("<p>Email successfully sent!</p>");
